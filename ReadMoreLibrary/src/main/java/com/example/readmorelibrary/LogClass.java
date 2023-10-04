d6754c3307f35faa3d1c599a27843a32ef81c8f6
Carina Tiemi Yoshida <carina.yoshida@catskillet.com>
2023-10-04 13:52:38 -0400

    feat: library of test

---
+++ ReadMoreLibrary/src/main/java/com/example/readmorelibrary/LogClass.java
@@ -0,0 +1,15 @@
+package com.example.readmorelibrary;
+
+import android.util.Log;
+
+public class LogClass {
+    public static void main(String[] args) {
+        //Main Method
+        Log.e("========", "minha mensagem é esta!!");
+    }
+
+    public static void printlog(String msg) {
+        //Main Method
+        Log.e("========", msg);
+    }
+}
